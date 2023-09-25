fd11f068a9c765d0281831bb5a5e0fb0cd86e416
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-09-25 11:26:29 -0400

    product page styled added

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab possimus
+          a assumenda aperiam ipsam quisquam reprehenderit, explicabo
+          praesentium porro perferendis laboriosam veritatis ipsa aut in
+          temporibus obcaecati quos. Adipisci, itaque!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
