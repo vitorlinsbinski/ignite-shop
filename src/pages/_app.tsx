79245cc8ac12e0ef76dfa70e0b8e9cd66c012ea9
Vitor Linsbinski de Oliveira <vitor.linsbinski@hotmail.com>
2023-10-03 10:36:55 -0400

    changes in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,16 @@ import Head from "next/head";
 
 import { globalStyles } from "../styles/global";
 
-import { useContext } from "react";
-
 import { Header } from "../components/Header";
 import { Container } from "../styles/pages/app";
 
-import { CartContext, CartContextProvider } from "@/contexts/CartContext";
-import { LoadingComponent } from "@/components/LoadingComponent";
+import { CartContextProvider } from "@/contexts/CartContext";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { isCreatingCheckoutSection } = useContext(CartContext);
   return (
     <CartContextProvider>
-      {isCreatingCheckoutSection && <LoadingComponent />}
-
       <Container>
         <Header />
         <Component {...pageProps} />
